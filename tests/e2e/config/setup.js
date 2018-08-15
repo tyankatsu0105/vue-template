fa9d25a09a2f4e91a519d0ed3cc990790a5a2117
山本 勝也 <frips.rysilufupe@gmail.com>
2018-08-15 23:40:56 +0900

    fix: :+1: timeoutを引き伸ばした

--- tests/e2e/config/setup.js
+++ tests/e2e/config/setup.js
@@ -1,4 +1,5 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
+jest.setTimeout(3000000);
 expect.extend({ toMatchImageSnapshot });
 module.exports = require('expect-puppeteer');
